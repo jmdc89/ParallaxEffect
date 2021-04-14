1771506ba740f24260fa69c7c0643875417d95fb
Jose Manuel <jmdcarq89@gmail.com>
2021-04-14 20:06:56 +0200

    ParHooks styles

--- src/components/ParHooks.js
+++ src/components/ParHooks.js
@@ -1,8 +1,23 @@
 import React from "react";
 import styled from "styled-components";
+import "../styles/ParHooks.scss";
+
+const ContentHeading = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 1080px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 1;
+  margin-top: 100px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+`;
 
 function ParHooks() {
-  return <h1>hello from ParHooks</h1>;
+  return <ContentHeading></ContentHeading>;
 }
 
 export default ParHooks;
